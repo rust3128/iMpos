5bef253e2c1275f87d607ef6ed2d3ab87b8f5ec9
rust3128 <rust3128@gmail.com>
2019-07-11 16:08:19 +0300

    LoggingCategories

--- iMpos.pro
+++ iMpos.pro
@@ -4,7 +4,7 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       += core gui sql
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -25,10 +25,12 @@ DEFINES += QT_DEPRECATED_WARNINGS
 CONFIG += c++11
 
 SOURCES += \
+        LoggingCategories/loggingcategories.cpp \
         main.cpp \
         mainwindow.cpp
 
 HEADERS += \
+        LoggingCategories/loggingcategories.h \
         mainwindow.h
 
 FORMS += \
--- main.cpp
+++ main.cpp
@@ -1,11 +1,64 @@
 #include "mainwindow.h"
+#include "LoggingCategories/loggingcategories.h"
 #include <QApplication>
+#include <QFile>
+#include <QDateTime>
+
+// Умный указатель на файл логирования
+static QScopedPointer<QFile>   m_logFile;
+
+// Объявление обработчика
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Устанавливаем файл логирования
+    m_logFile.reset(new QFile("iMpos.log"));
+    // Открываем файл логирования
+    m_logFile.data()->open(QFile::Append | QFile::Text);
+    // Устанавливаем обработчик
+    qInstallMessageHandler(messageHandler);
+    qInfo(logInfo()) << "Запуск программы.";
+
+
     MainWindow w;
     w.show();
 
     return a.exec();
+}
+
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    // Открываем поток записи в файл
+    QTextStream out(m_logFile.data());
+    QTextStream console(stdout);
+
+
+    // Записываем дату записи
+    out << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz ");
+    // По типу определяем, к какому уровню относится сообщение
+    switch (type)
+    {
+#ifdef QT_DEBUG
+    case QtInfoMsg:     out << "[INF] "; console << "Info:     " << msg << endl; break;
+    case QtDebugMsg:    out << "[DBG] "; console << "Debug:    " << msg << endl; break;
+    case QtWarningMsg:  out << "[WRN] "; console << "Warning:  " << msg << endl; break;
+    case QtCriticalMsg: out << "[CRT] "; console << "Critical: " << msg << endl; break;
+    case QtFatalMsg:    out << "[FTL] "; console << "Fatality: " << msg << endl; break;
+#else
+    case QtInfoMsg:     out << "[INF] "; break;
+    case QtDebugMsg:    out << "[DBG] "; break;
+    case QtWarningMsg:  out << "[WRN] "; break;
+    case QtCriticalMsg: out << "[CRT] "; break;
+    case QtFatalMsg:    out << "[FTL] "; break;
+#endif
+
+    }
+    // Записываем в вывод категорию сообщения и само сообщение
+    out << context.category << ": " << msg << endl;
+    // Очищаем буферизированные данные
+    out.flush();
+    console.flush();
 }
